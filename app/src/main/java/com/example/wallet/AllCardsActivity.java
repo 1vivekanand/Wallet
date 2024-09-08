7fb79b08bddf76cbf56defdeea2d252b7e5a9ee4
1vivekanand <1vivekanandbhardwaj@gmail.com>
2024-09-08 22:42:04 +0530

    building expenses cards

--- app/src/main/java/com/example/wallet/AllCardsActivity.java
+++ app/src/main/java/com/example/wallet/AllCardsActivity.java
@@ -1,24 +1,33 @@
 package com.example.wallet;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class AllCardsActivity extends AppCompatActivity {
 
+
+    CardView expenseCard;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_all_cards);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        expenseCard=findViewById(R.id.cardExpenses);
+        expenseCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AllCardsActivity.this,ExpenseActivity.class));
+            }
         });
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,6 +9,7 @@ pluginManagement {
         }
         mavenCentral()
         gradlePluginPortal()
+        maven(url = "https://jitpack.io")
     }
 }
 dependencyResolutionManagement {
@@ -16,9 +17,9 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven(url = "https://jitpack.io") // Add JitPack here
     }
 }
 
 rootProject.name = "Wallet"
 include(":app")
- 
